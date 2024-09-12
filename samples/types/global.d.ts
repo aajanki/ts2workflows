845ded610b5c148065d29993aa3f3468f16da105
Antti Ajanki <antti.ajanki@iki.fi>
2024-09-12 21:07:29 +0300

    Global types have no properties in workflows

---
+++ samples/types/global.d.ts
@@ -0,0 +1,14 @@
+export {}
+
+declare global {
+  interface Array<T> {}
+  interface Boolean {}
+  interface CallableFunction {}
+  interface Function {}
+  interface IArguments {}
+  interface NewableFunction {}
+  interface Number {}
+  interface Object {}
+  interface RegExp {}
+  interface String {}
+}
