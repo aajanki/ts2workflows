eee70b20f5dc6b635c0fb6c84ed0df1f7b488596
Antti Ajanki <antti.ajanki@iki.fi>
2026-03-18 20:06:57 +0200

    remove debug logging

--- src/transpiler/linker.ts
+++ src/transpiler/linker.ts
@@ -8,10 +8,6 @@ export function findCalledFunctionDeclarations(
   // First add top level function declarations
   const declarations = rootNode.statements.filter(ts.isFunctionDeclaration)
 
-  console.debug(
-    `root declarations: ${declarations.map((x) => x.name?.getText().toString()).join(', ')}`,
-  )
-
   // Next, find nested function calls recursively
   findFunctionsRecursively(declarations, typeChecker, rootNode)
 
@@ -78,10 +74,6 @@ function functionDeclarationsForIdentifier(
   const isAliased = symbol && symbol.flags & ts.SymbolFlags.Alias
   const symbol2 = isAliased ? typeChecker.getAliasedSymbol(symbol) : symbol
   const declarations = symbol2?.getDeclarations() ?? []
-
-  console.debug(
-    `${node.getText()} - ${symbol2?.name}: kind = ${declarations.map((x) => x.kind.toString()).join(', ')}`,
-  )
 
   return declarations.filter(ts.isFunctionDeclaration)
 }
